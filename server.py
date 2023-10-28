dee1fb46ef5095ab91e2d0a8d73c45e513670f64
Rahil Shah <rahilshah@crc-dot1x-nat-10-239-46-139.bu.edu>
2023-10-28 18:08:38 -0400

    updated file changes

--- server.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-# visit the homepage and you have to sign in with bu.edu email
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
